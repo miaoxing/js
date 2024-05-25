cccd5ef23b9e6d2286985123a62f66b11d6c876b
twinh <twinhuang@qq.com>
2024-05-25 11:38:38 +0800

    feat(experimental): 移除 `$.fullUrl` 方法

--- index.ts
+++ index.ts
@@ -149,12 +149,6 @@ const $ = {
   url: (): string => {
     return '';
   },
-  /**
-   * @experimental
-   */
-  fullUrl: (): string => {
-    return '';
-  },
   apiUrl: (): string => {
     return '';
   },
